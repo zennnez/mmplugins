d240176b11026aa919eed5c7c045936f0bcb5eee
sudeep duhoon <rishi.gothic@gmail.com>
2022-05-26 19:34:01 +0530

    sticky: fixed derp and improved

--- sticky/sticky.py
+++ sticky/sticky.py
@@ -1,5 +1,4 @@
 import discord
-import asyncio
 import calendar
 import time
 from discord.ext import commands
@@ -10,11 +9,53 @@ from core.models import PermissionLevel
 class Sticky(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.db = bot.plugin_db.get_partition(self)
+        self.db = bot.api.get_plugin_partition(self)
         self.allowed_mentions = discord.AllowedMentions(users=True, roles=False, everyone=False)
 
-    async def check_msg(self, channel: discord.TextChannel, msg: discord.Message):
-        my_perms: discord.Permissions = channel.permissions_for(msg.guild.me)
+    async def send_sticky(self, channel, author, content, embed, attachments):
+        if not attachments:
+            attachments = ""
+
+        webhook = None
+        my_perms: discord.Permissions = channel.permissions_for(channel.guild.me)
+        if my_perms.manage_webhooks:
+            webhooks = await channel.webhooks()
+            webhook = discord.utils.get(webhooks, name=self.bot.user.name, user=self.bot.user)
+            if webhook is None:
+                webhook = await channel.create_webhook(name=self.bot.user.name)
+
+
+        if embed:
+            if webhook:
+                msg2 = await webhook.send(
+                    content,
+                    embed=embed,
+                    files=attachments,
+                    avatar_url=author.avatar_url,
+                    username=author.display_name,
+                    allowed_mentions=self.allowed_mentions,
+                    wait=True,
+                )
+            else:
+                msg2: discord.Message = await channel.send(content, embed=embed)
+
+        else:
+            if webhook:
+                msg2 = await webhook.send(
+                    content,
+                    files=attachments,
+                    avatar_url=author.avatar_url,
+                    username=author.display_name,
+                    allowed_mentions=self.allowed_mentions,
+                    wait=True,
+                )
+            else:
+                msg2: discord.Message = await channel.send(content)
+
+        return msg2
+
+    async def check_msg(self, channel: discord.TextChannel, msg: discord.Message, author):
+        author = author or msg.author
 
         if msg.content:
             content = msg.content
@@ -22,43 +63,26 @@ class Sticky(commands.Cog):
             content = ""
 
         if len(msg.attachments) > 0:
-            attachment = msg.attachments[0]
-        else:
-            attachment = ""
+            attachments = [await attachment.to_file() for attachment in msg.attachments if attachment.size <= 8000000]
 
-        if my_perms.manage_webhooks:
-            webhooks = await channel.webhooks()
-            webhook = discord.utils.get(webhooks, name=self.bot.user.name, user=self.bot.user)
-            if webhook is None:
-                webhook = await channel.create_webhook(name=self.bot.user.name)
-            if msg.embeds:
-                e = msg.embeds[0]
-                msg2 = await webhook.send(
-                    content,
-                    files=attachment,
-                    embed=e,
-                    avatar_url=msg.author.display_avatar.url,
-                    username=msg.author.display_name,
-                    allowed_mentions=self.allowed_mentions,
-                    wait=True,
-                )
-            else:
-                msg2 = await webhook.send(
-                    content,
-                    files=attachment,
-                    avatar_url=msg.author.display_avatar.url,
-                    username=msg.author.display_name,
-                    allowed_mentions=self.allowed_mentions,
-                    wait=True,
-                )
+            bad_attachments = [f'`<Bad File: {attachment.filename} | File Size: {attachment.size}>`' for attachment in msg.attachments if attachment.size > 8000000]
+            if bad_attachments:
+                if content:
+                    content += '\n'
+                content += '\n'.join(bad_attachments)
         else:
-            if msg.embeds:
-                e = msg.embeds[0]
-                msg2: discord.Message = await channel.send(f"{attachment}{content}", embed=e)
-            else:
-                msg2: discord.Message = await channel.send(f"{attachment}{content}")
+            attachments = ""
 
-        return msg2
+        if msg.embeds:
+            e = msg.embeds[0]
+            e_dict = e.to_dict()
+        else:
+            e = None
+            e_dict = None
+
+        msg2 = await self.send_sticky(channel, author, content, e, attachments)
+
+        return msg2, content, e_dict
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
@@ -79,54 +103,85 @@ class Sticky(commands.Cog):
 
         channel = message.channel
 
-        data = await self.db.find_one({'guild_id':message.guild.id, 'channel_id':channel.id})
-
+        data = self.db.find({'guild_id':message.guild.id, 'channel_id':channel.id})
         if not data:
             return
 
-        if data['enabled'] is False:
-            return
-
-        data = self.db.find({'guild_id':message.guild.id, 'channel_id':channel.id})
         async for sticky in data:
+            if sticky['enabled'] is False:
+                continue
+
             if sticky['counter'] >= (sticky['max_counter']-1):
                 if (calendar.timegm(time.gmtime()) - sticky['msg_time']) > sticky['cooldown']:
                     try:
-                        msg_check = await channel.fetch_message(sticky['msg_id'])
-                        await msg_check.delete()
+                        msg = await channel.fetch_message(sticky['msg_id'])
+                        await msg.delete()
 
-                    except discord.HTTPException:
-                        await self.db.delete_one({'guild_id':message.guild.id, 'channel_id':channel.id, 'msg_id':sticky['msg_id']})
-                        await self.bot.log_channel.send(f"Sticky msg failed in {channel.mention}")
-            
                     except discord.NotFound:
-                        await self.db.delete_one({'guild_id':message.guild.id, 'channel_id':channel.id, 'msg_id':sticky['msg_id']})
-                        await self.bot.log_channel.send(f"Sticky msg not found in {channel.mention}")
+                        if e_dict:= sticky['msg']['embed_dict']:
+                            e = discord.Embed.from_dict(e_dict)
+                        else:
+                            e = None
+
+                        content = sticky['msg']['content']
+                        
+                        author = message.guild.get_member(sticky['author']) or await message.self.bot.fetch_user(sticky['author'])
+                        msg = await self.send_sticky(channel, author, content, e, None)
+                        await self.db.find_one_and_update(
+                            {'guild_id':message.guild.id, 'channel_id':channel.id, 'msg_id':sticky['msg_id']},
+                                {"$set":{
+                                    'msg_id':msg.id,
+                                    'counter':0,
+                                    'msg_time': calendar.timegm(time.gmtime()),
+                                }})
             else:
                 await self.db.update_many({'guild_id':message.guild.id, 'channel_id':channel.id}, {"$set":{'counter':sticky['counter']+1}})
 
     @commands.Cog.listener()
     async def on_raw_message_delete(self, payload: discord.raw_models.RawMessageDeleteEvent):
-        channel = self.bot.get_channel(payload.channel_id)
-        data = await self.db.find_one({'guild_id':payload.guild_id, 'channel_id':channel.id, 'msg_id':payload.message_id})
+        data = await self.db.find_one({'guild_id':payload.guild_id, 'channel_id':payload.channel_id, 'msg_id':payload.message_id})
 
         if not data:
             return
 
+        channel = self.bot.get_channel(payload.channel_id)
+        guild = self.bot.get_guild(payload.guild_id)
+        author = guild.get_member(data['author']) or await self.bot.fetch_user(data['author'])
+
         try:
             msg_check = (payload.cached_message if payload.cached_message else await channel.fetch_message(payload.message_id))
         except discord.NotFound:
-            await self.db.delete_one({'guild_id':payload.guild_id, 'channel_id':channel.id, 'msg_id':payload.message_id})
-            return await self.bot.log_channel.send(f"Could not find deleted sticky message in {channel.mention}")
+            if e_dict:= data['msg']['embed_dict']:
+                e = discord.Embed.from_dict(e_dict)
+            else:
+                e = None
+
+            content = data['msg']['content']
+            msg_check = await self.send_sticky(channel, author, content, e, None)
+
+            await self.db.find_one_and_update(
+                {'guild_id':payload.guild_id, 'channel_id':payload.channel_id, 'msg_id':payload.message_id},
+                    {"$set":{
+                        'msg_id':msg_check.id,
+                        'counter':0,
+                        'msg_time': calendar.timegm(time.gmtime()),
+                    }})
+            return
         except discord.Forbidden:
-            await self.db.delete_one({'guild_id':payload.guild_id, 'channel_id':channel.id, 'msg_id':payload.message_id})
-            return await self.bot.log_channel.send(f"Failed to fetch deleted message due to permissions issue")
+            #await self.db.delete_one({'guild_id':payload.guild_id, 'channel_id':channel.id, 'msg_id':payload.message_id})
+            return await self.bot.log_channel.send(f"Failed to fetch sticky message in {channel.mention} due to permissions issue.")
 
-        msg: discord.Message = await self.check_msg(channel, msg_check)
+        msg, content, e_dict = await self.check_msg(channel, msg_check, author)
 
-        await self.db.find_one_and_update({'guild_id':payload.guild_id, 'channel_id':channel.id, 'msg_id':payload.message_id}, {"$set":{'msg_id':msg.id, 'counter':0, 'msg_time':calendar.timegm(time.gmtime())}})
+        await self.db.find_one_and_update(
+            {'guild_id':payload.guild_id, 'channel_id':payload.channel_id, 'msg_id':payload.message_id},
+                {"$set":{
+                    'msg_id':msg.id,
+                    'counter':0,
+                    'msg_time': calendar.timegm(time.gmtime()),
+                }})
 
-    @commands.group(name="stick", usage="<option>", invoke_without_command=True)
+    @commands.group(name="stick", usage="<counter> <cooldown (default is 30 seconds)>", invoke_without_command=True)
     @checks.has_permissions(PermissionLevel.MOD)
     async def stick_(self, ctx: commands.Context, max_counter: int, cooldown: Optional[int] = 30):
         """
@@ -134,23 +189,29 @@ class Sticky(commands.Cog):
 
         **Usage:**
         Reply to a message: `{prefix}stick`
+        `counter`: sticky message will be posted again after counter number of messages are sent to the channel
+        `cooldown`: sticky message will be posted only if countdown is over
         """
         if ctx.author == self.bot.user:
             return
         if ctx.author.bot:
             return
         if not ctx.invoked_subcommand:
-            embed = discord.Embed(timestamp=ctx.message.created_at, color=self.bot.main_color)
             try:
                 if ctx.message.reference:
                     msg_check = await ctx.channel.fetch_message(ctx.message.reference.message_id)
                     if not (msg_check and isinstance(msg_check, discord.Message)):
                         return
-                    msg = await self.check_msg(ctx.channel, msg_check)
+                    msg, content, e_dict = await self.check_msg(ctx.channel, msg_check, ctx.message.reference.resolved.author)
                     await self.db.insert_one({
                         'guild_id':ctx.guild.id,
                         'msg_id':msg.id,
+                        'msg':{
+                            'embed_dict':e_dict,
+                            'content':content,
+                            },
                         'channel_id':ctx.channel.id,
+                        'author':ctx.message.reference.resolved.author.id,
                         'counter':0,
                         'max_counter':max_counter,
                         'msg_time': calendar.timegm(time.gmtime()),
@@ -189,7 +250,7 @@ class Sticky(commands.Cog):
 
     @stick_.command(name="toggle")
     @checks.has_permissions(PermissionLevel.MOD)
-    async def stick_toggle(self, ctx: commands.Context, on_off: Optional[bool], channel: discord.TextChannel = None):
+    async def stick_toggle(self, ctx: commands.Context, on_off: Optional[bool] = False, channel: discord.TextChannel = None):
         """
         Toggle Sticky on/off.
 
