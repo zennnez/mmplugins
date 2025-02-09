477da9d987570822faab344d44db9ac1d4d77988
zen <rishi.gothic@gmail.com>
2025-02-09 19:08:46 +0530

    Update tstatusembed.py

--- tstatusembed/tstatusembed.py
+++ tstatusembed/tstatusembed.py
@@ -100,7 +100,7 @@ class TStatusEmbed(commands.Cog):
             thread = await self.bot.threads.find(recipient=recipient)
 
         else:
-            status = "Awaiting Member's Reply"
+            status = "Awaiting Staff's Reply"
             thread = await self.bot.threads.find(channel=channel)
 
         if not thread:
